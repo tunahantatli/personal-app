7d75e53b6ea1dd48aa7a786bdd0b2c8c1a4ccb4b
Tunahan Tatli <tunahantatl@gmail.com>
2023-01-14 13:35:06 +0300

    drf&swagger-installed

--- main/settings.py
+++ main/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # third party apps:
+    'drf_yasg',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
